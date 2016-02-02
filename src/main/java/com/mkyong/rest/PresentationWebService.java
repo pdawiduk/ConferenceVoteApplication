5098fb278a4187b429d7516966abdc8d5cbc89e3
pdawiduk <pdawiduk@gmail.com>
2016-02-02 12:12:05 +0100

    this work !!

--- src/main/java/com/mkyong/rest/PresentationWebService.java
+++ src/main/java/com/mkyong/rest/PresentationWebService.java
@@ -1,67 +1,70 @@
 package com.mkyong.rest;
- import java.sql.*;
+
+import java.sql.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
- 
+
 @Path("/presentation")
 public class PresentationWebService {
-    
+
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/presentations";
+    Connection conn = null;
+    Statement stmt = null;
 
     //  Database credentials
     static final String USER = "root";
     static final String PASS = "root";
- 
-	@GET
-        @Path("/show")
-        @Produces(MediaType.APPLICATION_JSON)
-	
-	public Response getMsg() throws SQLException {
-            
-                String query = "SELECT * FROM PRESENTATIONS ORDER BY room DESC";
-                        Statement stmt = null;
-                        ResultSet rs = stmt.executeQuery(query);
-                        ResultSetMetaData rsmd = rs.getMetaData();
-                        StringBuffer strJson = null;
-                        
-                        while(rs.next()){
-                            //Retrieve by column name
-                            int id  = rs.getInt("id");
-                            String presentation =rs.getString("presentation");
-                            String presenter = rs.getString("presenter");
-                            Float start = rs.getFloat("start");
-                            String desc = rs.getString("desription");
-                            int room =rs.getInt("room");
+    StringBuffer strJson = new StringBuffer();
+    
 
-                            
-                            //Display values
-                            strJson.append('"'+'{'+'"'+"presentations:["+'"');
-                            strJson.append("{");
-                            strJson.append('"'+ "id"+'"'+ '"'+id+'"');
-                            strJson.append('"'+ "presentation"+'"'+ '"'+presentation+'"');
-                            strJson.append('"'+ "presenter"+'"'+ '"'+presenter+'"');
-                            strJson.append('"'+ "start"+'"'+ '"'+start+'"');
-                            strJson.append('"'+ "description"+'"'+ '"'+desc+'"');
-                            strJson.append('"'+ "room"+'"'+ '"'+room+'"');
-                            strJson.append('"'+ "start"+'"'+ '"'+start+'"');
-                            if(!rs.next())
-                            strJson.append(',');
-                               
-                        }
-                        
+    @GET
+    @Path("/show")
+    @Produces(MediaType.APPLICATION_JSON)
 
-                        strJson.append("]}");
-                        rs.close();
- 
-		
- 
-		return Response.status(200).entity(strJson.toString()).build();
- 
-	}
- 
+    public Response getMsg() throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        stmt = conn.createStatement();
+        stmt.execute("USE presentations");
+        String query = "SELECT * FROM PRESENTATIONS;";
+        
+        ResultSet rs = stmt.executeQuery(query);
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        while (rs.next()) {
+
+            int id = rs.getInt("id");
+            String presentation = rs.getString("presentation");
+            String presenter = rs.getString("presenter");
+            Float start = rs.getFloat("start");
+            String desc = rs.getString("description");
+            String room = rs.getString("room");
+
+            strJson.append('"' + '{' + '"' + "presentations:[" + '"');
+            strJson.append("{");
+            strJson.append('"' + "id" + '"' + '"' + id + '"');
+            strJson.append('"' + "presentation" + '"' + '"' + presentation + '"');
+            strJson.append('"' + "presenter" + '"' + '"' + presenter + '"');
+            strJson.append('"' + "start" + '"' + '"' + start + '"');
+            strJson.append('"' + "description" + '"' + '"' + desc + '"');
+            strJson.append('"' + "room" + '"' + '"' + room + '"');
+
+            if (!rs.next()) {
+                strJson.append(',');
+            }
+
+        }
+
+        strJson.append("]}");
+        rs.close();
+        return Response.status(200).entity(strJson.toString()).build();
+
+    }
 }
