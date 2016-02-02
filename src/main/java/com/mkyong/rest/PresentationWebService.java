8617ed2368e5e42475b409d6095721f6c70a1a74
pdawiduk <pdawiduk@gmail.com>
2016-02-02 17:24:42 +0100

    !

--- src/main/java/com/mkyong/rest/PresentationWebService.java
+++ src/main/java/com/mkyong/rest/PresentationWebService.java
@@ -1,6 +1,13 @@
 package com.mkyong.rest;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,12 +22,15 @@ public class PresentationWebService {
     static final String DB_URL = "jdbc:mysql://localhost:3306/presentations";
     Connection conn = null;
     Statement stmt = null;
+    List<presentation> presentList= new ArrayList<presentation> ();
 
     //  Database credentials
     static final String USER = "root";
     static final String PASS = "root";
-    StringBuffer strJson = new StringBuffer();
-    
+
+    JsonObject albums = new JsonObject();
+    JsonArray datasets = new JsonArray();
+    JsonObject dataset = new JsonObject();
 
     @GET
     @Path("/show")
@@ -34,37 +44,25 @@ public class PresentationWebService {
         stmt = conn.createStatement();
         stmt.execute("USE presentations");
         String query = "SELECT * FROM PRESENTATIONS;";
-        
+
         ResultSet rs = stmt.executeQuery(query);
         ResultSetMetaData rsmd = rs.getMetaData();
 
-        while (rs.next()) {
-
-            int id = rs.getInt("id");
-            String presentation = rs.getString("presentation");
-            String presenter = rs.getString("presenter");
-            Float start = rs.getFloat("start");
-            String desc = rs.getString("description");
-            String room = rs.getString("room");
-
-            strJson.append('"' + '{' + '"' + "presentations:[" + '"');
-            strJson.append("{");
-            strJson.append('"' + "id" + '"' + '"' + id + '"');
-            strJson.append('"' + "presentation" + '"' + '"' + presentation + '"');
-            strJson.append('"' + "presenter" + '"' + '"' + presenter + '"');
-            strJson.append('"' + "start" + '"' + '"' + start + '"');
-            strJson.append('"' + "description" + '"' + '"' + desc + '"');
-            strJson.append('"' + "room" + '"' + '"' + room + '"');
-
-            if (!rs.next()) {
-                strJson.append(',');
-            }
-
+             
+        while (rs.next()) {    
+             presentList.add(new presentation(rs.getInt("id"),
+            rs.getString("presentation"),
+            rs.getString("presenter"),
+            rs.getFloat("start"),
+            rs.getString("description"),
+            rs.getString("room"),
+            rs.getDouble("vote")));
+       
         }
 
-        strJson.append("]}");
         rs.close();
-        return Response.status(200).entity(strJson.toString()).build();
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        return Response.status(200).entity(gson.toJson(presentList)).build();
 
     }
 }
