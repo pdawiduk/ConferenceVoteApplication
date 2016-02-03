aaedd3b6c6c8df540d18bca5d3cc3b27a3ada644
pdawiduk <pdawiduk@gmail.com>
2016-02-03 14:22:58 +0100

    add Database

---
+++ app/src/main/java/com/example/dawiduk/conferencevoteapplication/PresentationsInterface.java
@@ -0,0 +1,7 @@
+package com.example.dawiduk.conferencevoteapplication;
+
+/**
+ * Created by dawiduk on 3-2-16.
+ */
+public interface PresentationsInterface {
+}
---
+++ app/src/main/java/com/example/dawiduk/conferencevoteapplication/database/PresentationsDb.java
@@ -0,0 +1,7 @@
+package com.example.dawiduk.conferencevoteapplication.database;
+
+/**
+ * Created by dawiduk on 3-2-16.
+ */
+public class PresentationsDb {
+}
