dd8f338de157ea14651f6782f9e6a0c526354746
pdawiduk <pdawiduk@gmail.com>
2016-02-15 15:23:40 +0100

    creating racyclerview adapter

--- app/src/main/java/com/example/dawiduk/conferencevoteapplication/AgendaActivity.java
+++ app/src/main/java/com/example/dawiduk/conferencevoteapplication/AgendaActivity.java
@@ -1,28 +1,24 @@
 package com.example.dawiduk.conferencevoteapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.dawiduk.conferencevoteapplication.database.PresentationsDBstruct;
-import com.example.dawiduk.conferencevoteapplication.database.PresentationsDb;
 
 public class AgendaActivity extends AppCompatActivity {
 
@@ -94,7 +90,8 @@ public class AgendaActivity extends AppCompatActivity {
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
-        //    args.putStringArray(ARG_SECTION_NUMBER, presentations[sectionNumber-1]);
+            args.putStringArray(ARG_SECTION_NUMBER, presentations[sectionNumber-1]);
+            args.put
             fragment.setArguments(args);
             return fragment;
         }
@@ -110,7 +107,12 @@ public class AgendaActivity extends AppCompatActivity {
                     );
 
             View rootView = inflater.inflate(R.layout.fragment_agenda, container, false);
-            RecyclerView agendaRecycledView
+
+            RecyclerView agendaRecycledView = (RecyclerView) container.findViewById(R.id.presentation_list);
+            agendaRecycledView.setHasFixedSize(true);
+            agendaRecycledView.setLayoutManager(new LinearLayoutManager(getContext()));
+            agendaRecycledView.setItemAnimator(new DefaultItemAnimator());
+
             return rootView;
         }
     }
